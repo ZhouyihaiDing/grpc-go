2157e8cebb3523f18a97c00229cd7f0af506f52c
ZhouyihaiDing <ddyihai@google.com>
2017-06-22 15:22:29 -0700

    5

--- benchmark/c/run.sh
+++ benchmark/c/run.sh
@@ -16,17 +16,17 @@ if [ -d "benchmark/c" ]; then
   cat benchmark/c/result1
   git reset --hard ${commits[0]}
   ls benchmark/c/
-  if [ -e "benchmark/c/run.sh" ]; then
+  if [ -e "benchmark/c/main.go" ]; then
     echo "after reset: dir benchmark/c exist"
     go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Unary-Tracing-maxConcurrentCalls_64 | tee benchmark/compare/result2
     ls
     go run benchmark/compare/main.go benchmark/compare/result1 benchmark/compare/result2
   else
     echo "not exist"
-    mv benchmark/c/tmp benchmark/c/run.sh
+    mv benchmark/c/tmp benchmark/c/main.go
     ls
     go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Unary-Tracing-maxConcurrentCalls_64 | tee benchmark/compare/result2
     ls
-    go run benchmark/compare/main.go benchmark/compare/result1 benchmark/compare/result2
+    go run benchmark/c/main.go benchmark/c/result1 benchmark/c/result2
   fi
 fi
