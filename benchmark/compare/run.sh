bdad47a798ccc5c07b109640d33336132728a3b9
ZhouyihaiDing <ddyihai@google.com>
2017-06-22 15:04:50 -0700

    2

--- benchmark/compare/run.sh
+++ benchmark/compare/run.sh
@@ -10,14 +10,14 @@ echo "current commit number"
 echo ${commits[-1]}
 if [ -d "benchmark/compare" ]; then
   echo "dir benchmark/compare exist"
-  go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Tracing-kbps_0-MTU_0-maxConcurrentCalls_1-reqSize_1B-respSize_1B | tee benchmark/compare/result1
+  go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Unary-Tracing-maxConcurrentCalls_64 | tee benchmark/compare/result1
   ls benchmark/compare/
   cat benchmark/compare/result1
   git reset --hard ${commits[0]}
   ls benchmark/compare/
   if [ -d "benchmark/compare" ]; then
     echo "after reset: dir benchmark/compare exist"
-    go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Tracing-kbps_0-MTU_0-maxConcurrentCalls_1-reqSize_1B-respSize_1B | tee benchmark/compare/result2
+    go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/BenchmarkClient/Unary-Tracing-maxConcurrentCalls_64 | tee benchmark/compare/result2
     ls
     go run benchmark/compare/main.go benchmark/compare/result1 benchmark/compare/result2
   fi
