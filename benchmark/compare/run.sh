da9856cbfad427bf1980e4c2ca39200fe8adfb38
ZhouyihaiDing <ddyihai@google.com>
2017-06-26 11:57:30 -0700

    5

--- benchmark/compare/run.sh
+++ benchmark/compare/run.sh
@@ -12,22 +12,18 @@ if [[ $TRAVIS_GO_VERSION = 1.8* ]]; then
     echo "dir benchmark/compare exist"
     go get -d -v -t google.golang.org/grpc/...
 
-    cp benchmark/compare/main.go benchmark/compare/tmpmain
-    cp -r benchmark/stats benchmark/tmpstats
-    cp benchmark/benchmark17_test.go benchmark/tmp17test
+    cp -r benchmark tmpbenchmark
 
-    go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Unary-Tracing-kbps_0-MTU_0-maxConcurrentCalls_1 | tee benchmark/compare/result1
+    go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Unary-Tracing-kbps_0-MTU_0-maxConcurrentCalls_1 | tee result1
     ls benchmark/compare/
     git reset --hard ${commits[0]}
     ls benchmark/compare/
     if [ -e "benchmark/compare/main.go" ]; then
       echo "after reset: dir benchmark/compare exist"
     else
-      mv benchmark/tmpstats benchmark/stats
-      mv benchmark/tmp17test benchmark/benchmark17_test.go 
-      mv benchmark/compare/tmpmain benchmark/compare/main.go
+      mv tmpbenchmark benchmark
     fi
-    go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Tracing-kbps_0-MTU_0-maxConcurrentCalls_1 | tee benchmark/compare/result2
-    go run benchmark/compare/main.go benchmark/compare/result1 benchmark/compare/result2
+    go test google.golang.org/grpc/benchmark/... -benchmem -bench=BenchmarkClient/Tracing-kbps_0-MTU_0-maxConcurrentCalls_1 | tee result2
+    go run benchmark/compare/main.go result1 result2
   fi
 fi
